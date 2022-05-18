b636c52cf17de1a661e699a547dc7cbde4f58dcb
Eoinburke <burkeyeoin45@gmail.com>
2022-05-18 18:11:22 +0000

    API/home page working

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponce
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
-def main(request):
-    return HttpResponce("Hello")
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
