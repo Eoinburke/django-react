46e95612735ebc0d3801d383665645b277ab700a
Eoinburke <burkeyeoin45@gmail.com>
2022-07-19 17:18:17 +0000

    Added backend to room join page

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -6,41 +6,44 @@ export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    roomCode: "",
-    error: ""
+      roomCode: "",
+      error: "",
     };
     this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
-    this.roomButtonPressed = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
   }
 
   render() {
     return (
       <Grid container spacing={1}>
-        <Grid item xs={12} alignItems="center">
-          <Typography varient="h4" component="h4">
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
             Join a Room
           </Typography>
         </Grid>
-        <Grid item xs={12} alignItems="center">
+        <Grid item xs={12} align="center">
           <TextField
-          error={this.state.error}
-          label="Code"
-          placeholder="Enter a Room Code"
-          value={this.state.roomCode}
-          helperText={this.state.error}
-          varient="outlined"
-          onChange={this.handleTextFieldChange}
+            error={this.state.error}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this.handleTextFieldChange}
           />
-
         </Grid>
-        <Grid item xs={12} alignItems="center">
-          <Button varient="contained" color="primary" onClick={this.roomButtonPressed}>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.roomButtonPressed}
+          >
             Enter Room
-            </Button>
+          </Button>
         </Grid>
-        <Grid item xs={12} alignItems="center">
-        <Button varient="contained" color="secondary" to="/" component={Link}>
-          Back
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Back
           </Button>
         </Grid>
       </Grid>
@@ -49,9 +52,28 @@ export default class RoomJoinPage extends Component {
 
   handleTextFieldChange(e) {
     this.setState({
-      roomCode: e.target.value
+      roomCode: e.target.value,
     });
   }
 
-  roomButtonPressed()
+  roomButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: this.state.roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          this.props.history.push(`/room/${this.state.roomCode}`);
+        } else {
+          this.setState({ error: "Room not found." });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
